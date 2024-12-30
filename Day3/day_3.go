8eb07eb5c6e024c07a83ce753aabeac2ec842d30
Bozka222 <bochjan@seznam.cz>
2024-12-30 22:46:17 +0100

    Day3 completed

--- Day3/day_3.go
+++ Day3/day_3.go
@@ -1,11 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
 
 func main() {
-	hello()
-}
+	filePath := "Day3/input_data.txt"
 
-func hello() {
-	fmt.Println("Hello, World!")
+	//Read file
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+
+	fileContent := string(data)
+	//fmt.Println(fileContent)
+
+	//Regex string
+	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	matches := re.FindAllStringSubmatch(fileContent, -1)
+	//fmt.Println(matches)
+
+	totalSum := 0
+	for _, match := range matches { // When ranging over slice for returns (index, copy of element)
+		num1, _ := strconv.Atoi(match[1])
+		num2, _ := strconv.Atoi(match[2])
+		totalSum += num1 * num2
+	}
+	fmt.Printf("Total Sum: %d\n\n", totalSum)
+
 }
--- Day3/day_3_2.go
+++ Day3/day_3_2.go
@@ -1,1 +1,48 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	filePath := "input_data.txt"
+
+	//Read file
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+
+	fileContent := string(data)
+	//fmt.Println(fileContent)
+
+	//Regex string
+	mulRegex := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	doRegex := regexp.MustCompile(`do\(\)`)
+	dontRegex := regexp.MustCompile(`don't\(\)`)
+
+	instructions := regexp.MustCompile(`(?:(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)))`).FindAllString(fileContent, -1)
+
+	totalSum := 0
+	enabled := true
+	for _, instr := range instructions { // When ranging over slice for returns (index, copy of element)
+		switch {
+		case doRegex.MatchString(instr):
+			enabled = true
+		case dontRegex.MatchString(instr):
+			enabled = false
+		case mulRegex.MatchString(instr) && enabled:
+			match := mulRegex.FindStringSubmatch(instr)
+			num1, _ := strconv.Atoi(match[1])
+			num2, _ := strconv.Atoi(match[2])
+			totalSum += num1 * num2
+
+		}
+	}
+	fmt.Printf("Total Sum: %d\n\n", totalSum)
+
+}
